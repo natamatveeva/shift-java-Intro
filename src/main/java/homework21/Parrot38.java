690df57a5ab4cf86981e26c7e3038e2ac8a897b2
Natalya Matveeva <matveeva.n.v@sbertech.ru>
2025-11-15 08:59:48 +0300

    task2

---
+++ src/main/java/homework21/Parrot38.java
@@ -0,0 +1,10 @@
+package main.java.homework21;
+
+public class Parrot38 {
+    public static void main(String[] args) {
+        int boa = 380;
+        System.out.println("Рост попугая: " + boa/38./100 + "м");
+        System.out.println("Рост мартышки: " + boa/5./100 + "м");
+        System.out.println("Рост слоненка: " + boa/2./100 + "м");
+    }
+}
