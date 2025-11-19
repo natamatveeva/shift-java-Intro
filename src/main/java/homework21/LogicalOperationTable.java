c6370d313e798a76bc890fbf77cf2f7bec044216
Natalya <64015987+natamatveeva@users.noreply.github.com>
2025-11-19 07:18:23 +0300

    Update LogicalOperationTable.java
    замена PQ на pq

--- src/main/java/homework21/LogicalOperationTable.java
+++ src/main/java/homework21/LogicalOperationTable.java
@@ -2,7 +2,7 @@ package main.java.homework21;
 
 public class LogicalOperationTable {
     public static void main(String[] args) {
-        boolean P = false, Q = false;
+        boolean p = false, q = false;
         System.out.println("P\t\tQ\t\tP AND Q\tP OR Q\tP XOR Q\tNOT P");
         System.out.println(p + "\t" + q + "\t" + (p & q) + "\t" + (p | q) + "\t" + (p ^ q) + "\t" + (!p));
         p = true;
