c16749eb439ccd29be14bd9c8b74990619dee795
Natalya Matveeva <matveeva.n.v@sbertech.ru>
2025-11-19 12:22:04 +0300

    добавила функцию lenghtCalc

--- src/main/java/homework22/MultiplicationTable.java
+++ src/main/java/homework22/MultiplicationTable.java
@@ -11,28 +11,29 @@ public class MultiplicationTable {
         int end = scanner.nextInt();
         System.out.print("Введите шаг: ");
         int step = scanner.nextInt();
-        int[][] multiTable = new int[6][6];
-        System.out.println(multiTable.length);
+        int arrLenght = lenghtCalc(start, end, step) + 1;
+        System.out.println(arrLenght);
+        int[][] multiTable = new int[arrLenght][arrLenght];
         int startCopy1 = start;
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i < multiTable.length; i++) {
             multiTable[0][i] = startCopy1;
             startCopy1 += step;
-            if (i == 5) {
+            if (i == (multiTable.length - 1)) {
                 multiTable[0][i] = end;
             }
         }
         System.out.println();
         int startCopy2 = start;
-        for (int i = 1; i < 6; i++) {
+        for (int i = 1; i < multiTable.length; i++) {
             multiTable[i][0] = startCopy2;
             startCopy2 += step;
-            if (i == 5) {
+            if (i == (multiTable.length - 1)) {
                 multiTable[i][0] = end;
             }
         }
-        for (int i = 1; i < 5; i++) {
+        for (int i = 1; i < (multiTable.length); i++) {
             int multiplier = multiTable[i][0];
-            for (int j = 1; j < 5; j++) {
+            for (int j = 1; j < (multiTable.length); j++) {
                 multiTable[i][j] = multiTable[0][j]*multiplier;
             }
         }
@@ -41,11 +42,21 @@ public class MultiplicationTable {
     }
 
     public static void printTable(int arr[][]) {
-        for (int i = 0; i < 6; i++) {
-            for (int j = 0; j < 6; j++) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
                 System.out.printf("%5d",arr[i][j] );
             }
             System.out.println();
         }
+    }
+    public static int lenghtCalc(int start, int end, int step) {
+        int counter = 1;
+        if (start <= end) {
+            while (start < end) {
+                start += step;
+                counter++;
+            }
+        }
+        return counter;
     }
 }
