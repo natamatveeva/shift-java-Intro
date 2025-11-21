41567333e22411b7eb55edab2e4fe139292ead9d
Natalya Matveeva <matveeva.n.v@sbertech.ru>
2025-11-21 17:34:56 +0300

    исправлена ошибка

--- src/main/java/homework231/StringProcessing.java
+++ src/main/java/homework231/StringProcessing.java
@@ -2,8 +2,8 @@ package main.java.homework231;
 
 import java.util.Scanner;
 
-public class String {
-    public static void main(String[] args) {
+public class StringProcessing {
+    public static void main(StringProcessing[] args) {
         Scanner console = new Scanner(System.in);
         System.out.println("Введите строку больше одного символа: ");
         String str1 = console.nextLine();
