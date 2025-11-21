6a333d04ab20b24f334d2279577b0a93939f86c9
Natalya Matveeva <matveeva.n.v@sbertech.ru>
2025-11-21 17:28:13 +0300

    создан файл с дз

---
+++ src/main/java/homework231/String.java
@@ -0,0 +1,14 @@
+package main.java.homework231;
+
+import java.util.Scanner;
+
+public class String {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите строку больше одного символа: ");
+        String str1 = console.nextLine();
+
+
+
+    }
+}
